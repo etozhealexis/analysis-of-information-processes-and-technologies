81a6edf32ae3f6a144f6dba5616e10e46432e910
etozhealexis <alexisworksya@yandex.ru>
2023-09-08 18:55:07 +0300

    task5 done

--- homework1/service/task5.py
+++ homework1/service/task5.py
@@ -1,3 +1,8 @@
-def itersectionOfDiagonales(*args):
+def itersectionOfDiagonales(x1, x2, x3, x4):
+    firstPossibleStraightLine = (x3[0] - x1[0]) / (x2[0] - x1[0]) == (x3[1] - x1[0]) / (x2[1] - x1[1])
+    secondPossibleStraightLine = (x4[0] - x2[0]) / (x3[0] - x2[0]) == (x4[1] - x2[0]) / (x3[1] - x2[1])
 
-    return args
+    if firstPossibleStraightLine or secondPossibleStraightLine:
+        return 0
+
+    return [(x1[0] + x3[0]) / 2, (x1[1] + x3[1]) / 2]
