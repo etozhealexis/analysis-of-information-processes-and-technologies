6022bbea91409bb459289c9965beccfe17ca5009
etozhealexis <alexisworksya@yandex.ru>
2023-09-12 10:34:39 +0300

    task5 redone

--- homework1/service/task5.py
+++ homework1/service/task5.py
@@ -1,10 +1,10 @@
 def itersectionOfDiagonales(x1, x2, x3, x4):
-    firstPossibleStraightLine = (x3[0] - x1[0]) / (x2[0] - x1[0]) == (x3[1] - x1[0]) / (x2[1] - x1[1])
-    secondPossibleStraightLine = (x4[0] - x2[0]) / (x3[0] - x2[0]) == (x4[1] - x2[0]) / (x3[1] - x2[1])
-    thirdPossibleStraightLine = (x1[0] - x3[0]) / (x4[0] - x3[0]) == (x1[1] - x3[0]) / (x4[1] - x3[1])
-    fourthPossibleStraightLine = (x2[0] - x4[0]) / (x1[0] - x4[0]) == (x2[1] - x4[0]) / (x1[1] - x4[1])
+    firstLine = (x4[0] - x1[0]) * (x2[0] - x1[0]) - (x4[1] - x1[1]) * (x2[1] - x1[1])
+    secondLine = (x4[0] - x2[0]) * (x3[0] - x2[0]) - (x4[1] - x2[1]) * (x3[1] - x2[1])
+    thirdLine = (x4[0] - x3[0]) * (x1[0] - x3[0]) - (x4[1] - x3[1]) * (x1[1] - x3[1])
+    fourthLine = (x1[0] - x3[0]) * (x2[0] - x3[0]) - (x1[1] - x3[1]) * (x2[1] - x3[1])
 
-    if firstPossibleStraightLine or secondPossibleStraightLine or secondPossibleStraightLine or thirdPossibleStraightLine:
-        return 0
+    if firstLine * secondLine * thirdLine * fourthLine > 0:
+        return 'Не прямоугольник'
 
     return [(x1[0] + x3[0]) / 2, (x1[1] + x3[1]) / 2]
